33bbbf40d8474b8d7ab58c2ad7fbcc34fd4df79e
Gauthier BABEAU <gbabeau@e3r9p2.42.fr>
2021-01-26 17:28:33 +0100

    push libft

--- main.c
+++ main.c
@@ -1,20 +1,94 @@
 #include "philosophers.h"
 
-static void* test(void *arg)
-{:
-printf("je suis un philosophers\n");
-printf("tu es un philosophers\n");
-return arg;
+static void talk_philo(int num, char *etat)
+{
+	int	time;
+	time = 0;
+	printf("%d %d %s\n", time ,num, etat);
 }
 
-int main()
+static void* test(void *arg)
 {
+	struct timeval time1;
+	struct timeval time2;
+	t_philosopher *philo;
+	philo = (t_philosopher*)arg;
+	while (1)
+	{
+	gettimeofday(&time1, DST_NONE);
+	pthread_mutex_lock(&philo->fork[0]);
+	pthread_mutex_lock(&philo->fork[1]);
+	talk_philo(philo->num, STR_EAT);
+	usleep(100);
+	pthread_mutex_unlock(&philo->fork[0]);
+	pthread_mutex_unlock(&philo->fork[1]);
+	talk_philo(philo->num, STR_SLEEP);
+	usleep(100);
+	talk_philo(philo->num, STR_THINK);
+	printf("je suis un philosophers\n");
+	usleep(100);
+	printf("tu es un philosophers\n");
+	pthread_mutex_unlock(arg);
+	}
+return arg;
+}
+void initialization_fork(int number_of_philosopher, t_philosopher* philosophers)
+{
+	int	i;
+	pthread_mutex_t			*fork;
+
+	fork = malloc(sizeof(pthread_mutex_t) * number_of_philosopher);
+	i = -1;
+	while (++i < number_of_philosopher)
+		pthread_mutex_init(&fork[i], NULL);
+	i = -1;
+	while (++i < number_of_philosopher)
+	{
+		philosophers[i].num = 1;
+		philosophers[i].fork[0] = fork[i];
+		philosophers[i].fork[1] = fork[(i + 1)%number_of_philosopher];
+	}
+}
+
+void	initialization_pthread(int number_of_philosopher, t_philosopher* philosophers)
+{
+	int	i;
+
+	i = -1;
+	while (++i < number_of_philosopher)
+		pthread_create(&philosophers[i].thread_id,NULL, test, philosophers[0].fork);
+}
+
+void	initialization_philosophers(int number_of_philosopher, t_all* all,
+						t_philosopher* philosophers, pthread_mutex_t *general)
+{
+	int i;
+
+	i = -1;
+	all->general = *general;
+	all->philosophers = malloc(sizeof(t_philosopher*)* number_of_philosopher);
+	while (++i < number_of_philosopher)
+		all->philosophers[i] = &philosophers[i];
+}
+
+int	main()
+{
+	t_all			all;
 	t_philosopher	*philosophers;
-	philosophers = malloc(sizeof(t_philosopher));
-	pthread_mutex_init(&a, NULL);
-	pthread_create(&philosophers[0].thread_id,NULL, test,NULL);
-	printf("je suis un philosophers2\n");
-	usleep(1000000);
-	printf("tu es un philosophers2\n");
+	pthread_mutex_t	*fork;
+	pthread_mutex_t	general;
+	int				i;
+
+	i = -1;
+	all.number_of_philosopher = 2;
+	philosophers = malloc(sizeof(t_philosopher) * all.number_of_philosopher);
+	pthread_mutex_init(&general, NULL);
+	while (++i < all.number_of_philosopher)
+		philosophers[i].general = general;
+	pthread_mutex_lock(&general);
+	initialization_philosophers(all.number_of_philosopher, &all, philosophers, &general);
+	initialization_fork(all.number_of_philosopher, philosophers);
+	initialization_pthread(all.number_of_philosopher, philosophers);
+	pthread_mutex_lock(&general);
 	return (0);
 }
--- philosophers.h
+++ philosophers.h
@@ -4,16 +4,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+# define STR_DIED "died"
+# define STR_EAT "is eating"
+# define STR_SLEEP "is sleeping"
+# define STR_THINK "is thinking"
 
 typedef struct	s_philosopher
 {
-				int etat;
-				pthread_t thread_id;
-				pthread_mutex_t	*fork;
+				int				num;
+				int				etat;
+				pthread_t		thread_id;
+				pthread_mutex_t	fork[2];
+				pthread_mutex_t	general;
 }				t_philosopher;
 
 
 typedef struct	s_all
 {
-		t_philosopher philosophers;
+	int				number_of_philosopher;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	t_philosopher	**philosophers;
+	pthread_mutex_t	general;
 }			t_all;
